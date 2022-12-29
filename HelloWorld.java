065ad1ffd7fd20f82db2c1fef31d86ce2da9e9a4
EC2 Default User <ec2-user@ip-172-31-94-88.ec2.internal>
2022-12-29 16:00:16 +0000

    new code hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,6 +14,11 @@ System.out.println("Devops");
 System.out.println("Devops");
 
 System.out.println("Devops");
+System.out.println("Devops");
+System.out.println("Devops");
+System.out.println("Devops");
+System.out.println("Devops");
+System.out.println("Devops");
 
 
 
