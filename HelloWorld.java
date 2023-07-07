dbbc2d7827452476933f37ec93ab0245d67a7cef
Ubuntu <ubuntu@ip-172-31-34-18.ap-south-1.compute.internal>
2023-07-07 06:06:09 +0000

    code changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,17 +5,6 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("jenkins");
-System.out.println("Devops");
-
-System.out.println("Devops");
-
-System.out.println("Devops");
-
-System.out.println("Devops");
-System.out.println("Devops");
-System.out.println("Devops");
 System.out.println("Devops");
 System.out.println("Devops");
 System.out.println("Devops");
